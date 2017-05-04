e2ad6dfbd7267486349e4300a4db297c3332af5a
shinji3 <renrakusaki2003@yahoo.co.jp>
2017-05-04 12:21:49 +0900

    sys.hを整形

--- core/common/sys.h
+++ core/common/sys.h
@@ -2,7 +2,7 @@
 // File : sys.h
 // Date: 4-apr-2002
 // Author: giles
-// Desc: 
+// Desc:
 //
 // (c) 2002 peercast.org
 // ------------------------------------------------
@@ -23,219 +23,215 @@
 #include <string.h>
 #include "common.h"
 
-#define RAND(a,b) (((a = 36969 * (a & 65535) + (a >> 16)) << 16) + \
+#define RAND(a, b) (((a = 36969 * (a & 65535) + (a >> 16)) << 16) + \
                     (b = 18000 * (b & 65535) + (b >> 16))  )
 extern char *stristr(const char *s1, const char *s2);
 extern char *trimstr(char *s);
 
-
 #define MAX_CGI_LEN 1024
+
 // ------------------------------------
 class String
 {
 public:
-	enum {
-		MAX_LEN = 256
-	};
+    enum {
+        MAX_LEN = 256
+    };
 
-	enum TYPE
-	{
-		T_UNKNOWN,
-		T_ASCII,
-		T_HTML,
-		T_ESC,
-		T_ESCSAFE,
-		T_META,
-		T_METASAFE,
-		T_BASE64,
-		T_UNICODE,
-		T_UNICODESAFE,
+    enum TYPE
+    {
+        T_UNKNOWN,
+        T_ASCII,
+        T_HTML,
+        T_ESC,
+        T_ESCSAFE,
+        T_META,
+        T_METASAFE,
+        T_BASE64,
+        T_UNICODE,
+        T_UNICODESAFE,
 #ifdef WIN32
-		T_SJIS, //JP-EX
+        T_SJIS, //JP-EX
 #endif
-	};
+    };
 
-	String() {clear();}
-	String(const char *p, TYPE t=T_ASCII) 
-	{
-		set(p,t);
-	}
+    String() {clear();}
+    String(const char *p, TYPE t=T_ASCII) 
+    {
+        set(p,t);
+    }
 
-	// set from straight null terminated string
-	void set(const char *p, TYPE t=T_ASCII) 
-	{
-		strncpy(data,p,MAX_LEN-1);
-		data[MAX_LEN-1] = 0;
-		type = t;
-	}
+    // set from straight null terminated string
+    void set(const char *p, TYPE t=T_ASCII) 
+    {
+        strncpy(data,p,MAX_LEN-1);
+        data[MAX_LEN-1] = 0;
+        type = t;
+    }
 
-	// set from quoted or unquoted null terminated string
-	void setFromString(const char *str, TYPE t=T_ASCII);
-	
-	// set from stopwatch
-	void setFromStopwatch(unsigned int t);
+    // set from quoted or unquoted null terminated string
+    void setFromString(const char *str, TYPE t=T_ASCII);
+    
+    // set from stopwatch
+    void setFromStopwatch(unsigned int t);
 
-	// set from time
-	void setFromTime(unsigned int t);
-
-
-	// from single word (end at whitespace)
-	void setFromWord(const char *str)
-	{
-		int i;
-		for(i=0; i<MAX_LEN-1; i++)
-		{
-			data[i] = *str++;
-			if ((data[i]==0) || (data[i]==' '))
-				break;
-		}
-		data[i]=0;
-	}
+    // set from time
+    void setFromTime(unsigned int t);
 
 
-	// set from null terminated string, remove first/last chars
-	void setUnquote(const char *p, TYPE t=T_ASCII) 
-	{
-		size_t slen = strlen(p);
-		if (slen > 2)
-		{
-			if (slen >= MAX_LEN) slen = MAX_LEN;
-			strncpy(data,p+1,slen-2);
-			data[slen-2]=0;
-		}else
-			clear();
-		type = t;
-	}
+    // from single word (end at whitespace)
+    void setFromWord(const char *str)
+    {
+        int i;
+        for(i=0; i<MAX_LEN-1; i++)
+        {
+            data[i] = *str++;
+            if ((data[i]==0) || (data[i]==' '))
+                break;
+        }
+        data[i]=0;
+    }
 
-	void clear() 
-	{
-		memset(data, 0, MAX_LEN);
-		data[0]=0;
-		type = T_UNKNOWN;
-	}
-	void ASCII2ESC(const char *,bool);
-	void ASCII2HTML(const char *);
-	void ASCII2META(const char *,bool);
-	void ESC2ASCII(const char *);
-	void HTML2ASCII(const char *);
-	void HTML2UNICODE(const char *);
-	void BASE642ASCII(const char *);
-	void UNKNOWN2UNICODE(const char *,bool);
+
+    // set from null terminated string, remove first/last chars
+    void setUnquote(const char *p, TYPE t=T_ASCII) 
+    {
+        size_t slen = strlen(p);
+        if (slen > 2)
+        {
+            if (slen >= MAX_LEN) slen = MAX_LEN;
+            strncpy(data,p+1,slen-2);
+            data[slen-2]=0;
+        }else
+            clear();
+        type = t;
+    }
+
+    void clear() 
+    {
+        memset(data, 0, MAX_LEN);
+        data[0]=0;
+        type = T_UNKNOWN;
+    }
+    void ASCII2ESC(const char *,bool);
+    void ASCII2HTML(const char *);
+    void ASCII2META(const char *,bool);
+    void ESC2ASCII(const char *);
+    void HTML2ASCII(const char *);
+    void HTML2UNICODE(const char *);
+    void BASE642ASCII(const char *);
+    void UNKNOWN2UNICODE(const char *,bool);
 #ifdef WIN32
-	void ASCII2SJIS(const char *); //JP-EX
+    void ASCII2SJIS(const char *); //JP-EX
 #endif
 
-	static	int	base64WordToChars(char *,const char *);
+    static    int    base64WordToChars(char *,const char *);
 
-	static bool isSame(const char *s1, const char *s2) {return strcmp(s1,s2)==0;}
+    static bool isSame(const char *s1, const char *s2) {return strcmp(s1,s2)==0;}
 
-	bool startsWith(const char *s) const {return strncmp(data,s,strlen(s))==0;}
-	bool isValidURL();
-	bool isEmpty() {return data[0]==0;}
-	bool isSame(::String &s) const {return strcmp(data,s.data)==0;}
-	bool isSame(const char *s) const {return strcmp(data,s)==0;}
-	bool contains(::String &s) {return stristr(data,s.data)!=NULL;}
-	bool contains(const char *s) {return stristr(data,s)!=NULL;}
-	void append(const char *s)
-	{
-		if ((strlen(s)+strlen(data) < (MAX_LEN-1)))
-			strcat(data,s);
-	}
-	void append(char c)
-	{
-		char tmp[2];
-		tmp[0]=c;
-		tmp[1]=0;
-		append(tmp);
-	}
+    bool startsWith(const char *s) const {return strncmp(data,s,strlen(s))==0;}
+    bool isValidURL();
+    bool isEmpty() {return data[0]==0;}
+    bool isSame(::String &s) const {return strcmp(data,s.data)==0;}
+    bool isSame(const char *s) const {return strcmp(data,s)==0;}
+    bool contains(::String &s) {return stristr(data,s.data)!=NULL;}
+    bool contains(const char *s) {return stristr(data,s)!=NULL;}
+    void append(const char *s)
+    {
+        if ((strlen(s)+strlen(data) < (MAX_LEN-1)))
+            strcat(data,s);
+    }
+    void append(char c)
+    {
+        char tmp[2];
+        tmp[0]=c;
+        tmp[1]=0;
+        append(tmp);
+    }
 
-	void prepend(const char *s)
-	{
-		::String tmp;
-		tmp.set(s);
-		tmp.append(data);
-		tmp.type = type;
-		*this = tmp;
-	}
+    void prepend(const char *s)
+    {
+        ::String tmp;
+        tmp.set(s);
+        tmp.append(data);
+        tmp.type = type;
+        *this = tmp;
+    }
 
- 	bool operator == (const char *s) const {return isSame(s);}
- 	bool operator != (const char *s) const {return !isSame(s);}
+     bool operator == (const char *s) const {return isSame(s);}
+     bool operator != (const char *s) const {return !isSame(s);}
 
-	operator const char *() const {return data;}
+    operator const char *() const {return data;}
 
-	void convertTo(TYPE t);
+    void convertTo(TYPE t);
 
-	char	*cstr() {return data;}
+    char    *cstr() {return data;}
 
-	static bool isWhitespace(char c) {return c==' ' || c=='\t';}
+    static bool isWhitespace(char c) {return c==' ' || c=='\t';}
 
-	TYPE	type;
-	char	data[MAX_LEN];
+    TYPE    type;
+    char    data[MAX_LEN];
 };
 
 // ------------------------------------
 namespace peercast {
 class Random {
 public:
-	Random(int s=0x14235465)
-	{
-		setSeed(s);
-	}
-	
-	unsigned int next()
-	{
-		return RAND(a[0],a[1]);
-	}
+    Random(int s=0x14235465)
+    {
+        setSeed(s);
+    }
 
-	void setSeed(int s)
-	{
-		a[0] = a[1] = s;
-	}	
-	
-	unsigned long a[2];
+    unsigned int next()
+    {
+        return RAND(a[0], a[1]);
+    }
+
+    void setSeed(int s)
+    {
+        a[0] = a[1] = s;
+    }
+
+    unsigned long a[2];
 };
 }
 // ------------------------------------
 class Sys
 {
 public:
-	Sys();
+    Sys();
 
-
-
-    virtual class ClientSocket	*createSocket() = 0;
-	virtual bool			startThread(class ThreadInfo *) = 0;
-	virtual void			sleep(int) = 0;
-	virtual void			appMsg(long,long = 0) = 0;
-	virtual unsigned int	getTime() = 0;		
-	virtual double			getDTime() = 0;		
-	virtual unsigned int	rnd() = 0;
-	virtual void			getURL(const char *) = 0;
-	virtual void			exit() = 0;
-	virtual bool			hasGUI() = 0;
-	virtual void			callLocalURL(const char *,int)=0;
-	virtual void			executeFile(const char *) = 0;
-	virtual void			endThread(ThreadInfo *) {}
-	virtual void			waitThread(ThreadInfo *, int timeout = 30000) {}
-
-
+    virtual class ClientSocket  *createSocket() = 0;
+    virtual bool            startThread(class ThreadInfo *) = 0;
+    virtual void            sleep(int) = 0;
+    virtual void            appMsg(long, long = 0) = 0;
+    virtual unsigned int    getTime() = 0;
+    virtual double          getDTime() = 0;
+    virtual unsigned int    rnd() = 0;
+    virtual void            getURL(const char *) = 0;
+    virtual void            exit() = 0;
+    virtual bool            hasGUI() = 0;
+    virtual void            callLocalURL(const char *, int)=0;
+    virtual void            executeFile(const char *) = 0;
+    virtual void            endThread(ThreadInfo *) {}
+    virtual void            waitThread(ThreadInfo *, int timeout = 30000) {}
 
 #ifdef __BIG_ENDIAN__
-	unsigned short	convertEndian(unsigned short v) { return SWAP2(v); }
-	unsigned int	convertEndian(unsigned int v) { return SWAP4(v); }
+    unsigned short  convertEndian(unsigned short v) { return SWAP2(v); }
+    unsigned int    convertEndian(unsigned int v) { return SWAP4(v); }
 #else
-	unsigned short	convertEndian(unsigned short v) { return v; }
-	unsigned int	convertEndian(unsigned int v) { return v; }
+    unsigned short  convertEndian(unsigned short v) { return v; }
+    unsigned int    convertEndian(unsigned int v) { return v; }
 #endif
 
 
-	void	sleepIdle();
+    void    sleepIdle();
 
-	unsigned int idleSleepTime;
-	unsigned int rndSeed;
-	unsigned int numThreads;
+    unsigned int idleSleepTime;
+    unsigned int rndSeed;
+    unsigned int numThreads;
 
-	class LogBuffer	*logBuf;
+    class LogBuffer *logBuf;
 };
 
 
@@ -252,47 +248,43 @@ typedef unsigned int uintptr_t;
 class WEvent
 {
 public:
-	WEvent()
-	{
-		 event = ::CreateEvent(NULL, // no security attributes
-                                  TRUE, // manual-reset
-                                  FALSE,// initially non-signaled
-                                  NULL);// anonymous
-	}
+    WEvent()
+    {
+        event = ::CreateEvent(NULL,  // no security attributes
+                              TRUE,  // manual-reset
+                              FALSE, // initially non-signaled
+                              NULL); // anonymous
+    }
 
-	~WEvent()
-	{
-		::CloseHandle(event);
-	}
+    ~WEvent()
+    {
+        ::CloseHandle(event);
+    }
 
-	void	signal()
-	{
-		::SetEvent(event);
-	}
+    void    signal()
+    {
+        ::SetEvent(event);
+    }
 
-	void	wait(int timeout = 30000)
-	{
-		switch(::WaitForSingleObject(event, timeout))
-		{
-          case WAIT_TIMEOUT:
-              throw TimeoutException();
-              break;
-          //case WAIT_OBJECT_0:
-              //break;
-		}
-	}
+    void    wait(int timeout = 30000)
+    {
+        switch(::WaitForSingleObject(event, timeout))
+        {
+        case WAIT_TIMEOUT:
+            throw TimeoutException();
+            break;
+        //case WAIT_OBJECT_0:
+            //break;
+        }
+    }
 
-	void	reset()
-	{
-		::ResetEvent(event);
-	}
+    void    reset()
+    {
+        ::ResetEvent(event);
+    }
 
-
-
-	HANDLE event;
+    HANDLE event;
 };
-
-
 
 // ------------------------------------
 typedef int (WINAPI *THREAD_FUNC)(ThreadInfo *);
@@ -304,23 +296,23 @@ typedef uintptr_t THREAD_HANDLE;
 class WLock
 {
 public:
-	WLock()
-	{
-		InitializeCriticalSection(&cs);
-	}
+    WLock()
+    {
+        InitializeCriticalSection(&cs);
+    }
 
 
-	void	on()
-	{
-		EnterCriticalSection(&cs);
-	}
+    void    on()
+    {
+        EnterCriticalSection(&cs);
+    }
 
-	void	off()
-	{
-		LeaveCriticalSection(&cs);
-	}
-	
-	CRITICAL_SECTION cs;
+    void    off()
+    {
+        LeaveCriticalSection(&cs);
+    }
+
+    CRITICAL_SECTION cs;
 };
 #endif
 
@@ -338,7 +330,7 @@ public:
 typedef long long int64_t;
 
 typedef int (*THREAD_FUNC)(ThreadInfo *);
-#define THREAD_PROC int 
+#define THREAD_PROC int
 typedef pthread_t THREAD_HANDLE;
 
 // ------------------------------------
@@ -351,170 +343,170 @@ class WEvent
 {
 public:
 
-	WEvent()
-	{
-	}
+    WEvent()
+    {
+    }
 
-	void	signal()
-	{
-	}
+    void    signal()
+    {
+    }
 
-	void	wait(int timeout = 30000)
-	{
-	}
+    void    wait(int timeout = 30000)
+    {
+    }
 
-	void	reset()
-	{
-	}
+    void    reset()
+    {
+    }
 
 };
 
 // ------------------------------------
-class WLock 
+class WLock
 {
 private:
-	pthread_mutex_t mutex;
+    pthread_mutex_t mutex;
 public:
-	WLock()
-	{
-	    const pthread_mutexattr_t mattr = 
-		{
-#ifdef __APPLE__		
-			PTHREAD_MUTEX_RECURSIVE
+    WLock()
+    {
+        const pthread_mutexattr_t mattr =
+        {
+#ifdef __APPLE__
+            PTHREAD_MUTEX_RECURSIVE
 #else
-			PTHREAD_MUTEX_RECURSIVE_NP
+            PTHREAD_MUTEX_RECURSIVE_NP
 #endif
-		 };
-		 
+         };
+
         pthread_mutex_init( &mutex, &mattr );
-	}
+    }
 
-	~WLock()
-	{
+    ~WLock()
+    {
         pthread_mutex_destroy( &mutex );
-	}
+    }
 
 
-	void	on()
-	{
-		pthread_mutex_lock(&mutex);
-	}
+    void    on()
+    {
+        pthread_mutex_lock(&mutex);
+    }
 
-	void	off()
-	{
-		pthread_mutex_unlock(&mutex);
-	}
-	
+    void    off()
+    {
+        pthread_mutex_unlock(&mutex);
+    }
+
 };
 #endif
 
 class WLockBlock
 {
 private:
-	WLock *lock;
-	bool flg;
+    WLock *lock;
+    bool flg;
 public:
-	WLockBlock(WLock *l){
-		lock = l;
-		flg = false;
-	}
-	~WLockBlock(){
-		if (flg){
-			lock->off();
-			LOG_DEBUG("LOCK OFF by destructor");
-		}
-	}
-	void on(){
-		flg = true;
-		lock->on();
-	}
-	void off(){
-		flg = false;
-		lock->off();
-	}
+    WLockBlock(WLock *l){
+        lock = l;
+        flg = false;
+    }
+    ~WLockBlock(){
+        if (flg){
+            lock->off();
+            LOG_DEBUG("LOCK OFF by destructor");
+        }
+    }
+    void on(){
+        flg = true;
+        lock->on();
+    }
+    void off(){
+        flg = false;
+        lock->off();
+    }
 };
 
 // ------------------------------------
 class ThreadInfo
 {
 public:
-	//typedef int  (__stdcall *THREAD_FUNC)(ThreadInfo *);
+    //typedef int  (__stdcall *THREAD_FUNC)(ThreadInfo *);
 
-	ThreadInfo()
-	{
-		active = false;
-		finish = false;
-		id = 0;
-		func = NULL;
-		data = NULL;
-	}
+    ThreadInfo()
+    {
+        active = false;
+        finish = false;
+        id = 0;
+        func = NULL;
+        data = NULL;
+    }
 
-	void	shutdown();
+    void    shutdown();
 
-	volatile bool	active;
-	volatile bool	finish;
-	int		id;
-	THREAD_FUNC func;
-	THREAD_HANDLE handle;
-	
+    volatile bool   active;
+    volatile bool   finish;
+    int             id;
+    THREAD_FUNC     func;
+    THREAD_HANDLE   handle;
 
-	void	*data;
+    void            *data;
 };
-
 
 // ------------------------------------
-class LogBuffer 
+class LogBuffer
 {
 public:
-	enum TYPE
-	{
-		T_NONE,
-		T_DEBUG,
-		T_ERROR,
-		T_NETWORK,
-		T_CHANNEL,
-	};
+    enum TYPE
+    {
+        T_NONE,
+        T_DEBUG,
+        T_ERROR,
+        T_NETWORK,
+        T_CHANNEL,
+    };
 
-	LogBuffer(int i, int l)
-	{
-		lineLen = l;
-		maxLines = i;
-		currLine = 0;
-		buf = new char[lineLen*maxLines];
-		times = new unsigned int [maxLines];
-		types = new TYPE [maxLines];
-	}
+    LogBuffer(int i, int l)
+    {
+        lineLen = l;
+        maxLines = i;
+        currLine = 0;
+        buf = new char[lineLen*maxLines];
+        times = new unsigned int [maxLines];
+        types = new TYPE [maxLines];
+    }
 
-	void	clear()
-	{
-		currLine = 0;
-	}
-	void	write(const char *, TYPE);
-	static const char *getTypeStr(TYPE t) {return logTypes[t];}
-	void	dumpHTML(class Stream &);
+    void    clear()
+    {
+        currLine = 0;
+    }
 
-	char *buf;
-	unsigned int *times;
-	unsigned int currLine,maxLines,lineLen;
-	TYPE	*types;
-	WLock	lock;
-	static	const char *logTypes[];
+    void                write(const char *, TYPE);
+    static const char   *getTypeStr(TYPE t) { return logTypes[t]; }
+    void                dumpHTML(class Stream &);
+
+    char            *buf;
+    unsigned int    *times;
+    unsigned int    currLine, maxLines, lineLen;
+    TYPE            *types;
+    WLock           lock;
+    static          const char *logTypes[];
 
 };
+
 
 #define RWLOCK_READ_MAX 32
 
 class LockBlock
 {
 public:
-	LockBlock(WLock &l){ flg = false; lock = l; }
-	~LockBlock(){ if (flg) lock.off(); }
-	void lockon(){ flg = true; lock.on(); }
-	void lockoff(){ flg = false; lock.off(); }
+    LockBlock(WLock &l){ flg = false; lock = l; }
+    ~LockBlock(){ if (flg) lock.off(); }
+    void lockon(){ flg = true; lock.on(); }
+    void lockoff(){ flg = false; lock.off(); }
 
 private:
-	WLock lock;
-	bool flg;
+    WLock lock;
+    bool flg;
 };
 
 // ------------------------------------
