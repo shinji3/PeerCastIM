59eef5e218b13ff8c4f40e81559e302177341f8e
shinji3 <renrakusaki2003@yahoo.co.jp>
2017-05-24 22:33:12 +0900

    testsにファイルを追加

---
+++ tests/notification_unittest.cpp
@@ -0,0 +1,51 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include "notif.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace NotificationFixture
+{
+    TEST_CLASS(NotificationFixture)
+    {
+    public:
+
+        TEST_METHOD(initialState1)
+        {
+            Notification notif;
+            Assert::AreEqual(0, (int)notif.time);
+            Assert::AreEqual((int)ServMgr::NT_PEERCAST, (int)notif.type);
+            Assert::AreEqual("", notif.message.c_str());
+        }
+
+        TEST_METHOD(initialState2)
+        {
+            Notification notif(1, ServMgr::NT_BROADCASTERS, "A");
+            Assert::AreEqual(1, (int)notif.time);
+            Assert::AreEqual((int)ServMgr::NT_BROADCASTERS, (int)notif.type);
+            Assert::AreEqual("A", notif.message.c_str());
+        }
+
+        TEST_METHOD(getTypeStr)
+        {
+            Notification notif;
+
+            notif.type = ServMgr::NT_UPGRADE;
+            Assert::AreEqual("Upgrade Alert", notif.getTypeStr().c_str());
+
+            notif.type = ServMgr::NT_PEERCAST;
+            Assert::AreEqual("Peercast", notif.getTypeStr().c_str());
+
+            notif.type = ServMgr::NT_BROADCASTERS;
+            Assert::AreEqual("Broadcasters", notif.getTypeStr().c_str());
+
+            notif.type = ServMgr::NT_TRACKINFO;
+            Assert::AreEqual("Track Info", notif.getTypeStr().c_str());
+
+            notif.type = (ServMgr::NOTIFY_TYPE)7144;
+            Assert::AreEqual("Unknown", notif.getTypeStr().c_str());
+        }
+
+    };
+}
---
+++ tests/stdstring_unittest.cpp
@@ -0,0 +1,59 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include <string>
+
+using namespace std;
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace stdStringFixture
+{
+    TEST_CLASS(stdStringFixture)
+    {
+    public:
+
+        TEST_METHOD(size)
+        {
+            const char *p = "a\0b";
+            Assert::AreEqual(3, (int)string(p, p + 3).size());
+
+            // char* ‚©‚ç‰Šú‰»‚·‚éê‡‚Í \0 ‚ÅØ‚ê‚éB
+            Assert::AreEqual(1, (int)string(p).size());
+        }
+
+        TEST_METHOD(equals)
+        {
+            Assert::IsTrue(string("a") != string("b"));
+            Assert::IsTrue(string("a") != string("ab"));
+            Assert::IsTrue(string("a\0b") == string("a"));
+            Assert::IsTrue(string("abc") == string("abc\0"));
+        }
+
+        TEST_METHOD(substr)
+        {
+            string s = "0123456789";
+
+            // 2‚Â–Ú‚Ìˆø”‚ÍˆÊ’u‚Å‚Í‚È‚­’·‚³B
+            Assert::AreEqual("23", s.substr(2, 2).c_str());
+        }
+
+        TEST_METHOD(find)
+        {
+            string s = "0123456789";
+
+            Assert::AreEqual(string::npos, s.find('A'));
+            Assert::AreEqual(-1, (int)string::npos);
+        }
+
+        TEST_METHOD(initializerList)
+        {
+            string s = { 0, 1 };
+
+            Assert::AreEqual(2, (int)s.size());
+            Assert::AreEqual(0, (int)s[0]);
+            Assert::AreEqual(1, (int)s[1]);
+        }
+
+    };
+}
