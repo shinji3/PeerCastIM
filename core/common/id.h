1909383ce0dbc722c912e28bb9147fc0e5bd9ed2
shinji3 <renrakusaki2003@yahoo.co.jp>
2017-07-18 12:42:36 +0900

    id.hを整形

--- core/common/id.h
+++ core/common/id.h
@@ -12,16 +12,16 @@ private:
         LENGTH = 31
     };
 
-    char data[LENGTH + 1];
+    char data[LENGTH+1];
 public:
     IDString(const char *id, int cnt)
     {
         if (cnt >= LENGTH)
-            cnt = LENGTH;
+            cnt=LENGTH;
         int i;
-        for (i = 0; i < cnt; i++)
-            data[i] = id[i];
-        data[i] = 0;
+        for (i=0; i<cnt; i++)
+            data[i]=id[i];
+        data[i]=0;
     }
 
     operator const char *()
@@ -30,11 +30,7 @@ public:
     }
 
     const char *str() { return data; }
-
 };
-
-
-
 
 // ---------------------------------------------------
 class ID4
@@ -42,28 +38,28 @@ class ID4
 private:
     union
     {
-        int	iv;
+        int iv;
         char cv[4];
     };
 
 public:
 
     ID4()
-        : iv(0)
+    : iv( 0 )
     {
     }
 
     ID4(int i)
-        :iv(i)
+    :iv(i)
     {
     }
 
     ID4(const char *id)
-        :iv(0)
+    :iv(0)
     {
         if (id)
-            for (int i = 0; i < 4; i++)
-                if ((cv[i] = id[i]) == 0)
+            for (int i=0; i<4; i++)
+                if ((cv[i]=id[i])==0)
                     break;
     }
 
@@ -79,23 +75,18 @@ public:
 
     int operator == (ID4 id) const
     {
-        return iv == id.iv;
+        return iv==id.iv;
     }
     int operator != (ID4 id) const
     {
-        return iv != id.iv;
+        return iv!=id.iv;
     }
 
-    bool	isSet() const { return iv != 0; }
+    bool        isSet() const { return iv!=0; }
 
-
-    int getValue() const { return iv; }
-    IDString getString() const { return IDString(cv, 4); }
-    void *getData() { return cv; }
-
+    int         getValue() const { return iv; }
+    IDString    getString() const { return IDString(cv, 4); }
+    void        *getData() { return cv; }
 };
-
-
-
 
 #endif
