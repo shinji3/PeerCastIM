70dd782c1ebdd72b35d9118de81faec829856830
shinji3 <renrakusaki2003@yahoo.co.jp>
2017-07-18 11:45:21 +0900

    asf.hを整形

--- core/common/asf.h
+++ core/common/asf.h
@@ -19,7 +19,6 @@
 #ifndef _ASF_H
 #define _ASF_H
 
-
 #include "stream.h"
 
 // -----------------------------------
@@ -43,7 +42,6 @@ public:
         out.write(data4, 8);
     }
 
-
     void toString(String &s)
     {
         snprintf(s.data, _countof(s.data), "%X-%X-%X-%02X%02X%02X%02X%02X%02X%02X%02X",
@@ -52,41 +50,38 @@ public:
             data4[4], data4[5], data4[6], data4[7]);
     }
 
-    int operator==(const MSID& msid) const { return !memcmp(this, &msid, sizeof(MSID)); }
+    int operator==(const MSID& msid) const
+    {
+        return !memcmp(this, &msid, sizeof(MSID));
+    }
 
-    unsigned int data1;
-    unsigned short data2, data3;
-    unsigned char data4[8];
-
+    unsigned int    data1;
+    unsigned short  data2, data3;
+    unsigned char   data4[8];
 };
 
-
-
-
 // -----------------------------------
-const MSID headObjID =
-{ 0x75B22630, 0x668E, 0x11CF, 0xA6,0xD9,0x00,0xAA,0x00,0x62,0xCE,0x6C };
-const MSID dataObjID =
-{ 0x75B22636, 0x668E, 0x11CF, 0xA6,0xD9,0x00,0xAA,0x00,0x62,0xCE,0x6C };
-const MSID filePropObjID =
-{ 0x8CABDCA1, 0xA947, 0x11CF, 0x8E,0xE4,0x00,0xC0,0x0C,0x20,0x53,0x65 };
-const MSID streamPropObjID =
-{ 0xB7DC0791, 0xA9B7, 0x11CF, 0x8E,0xE6,0x00,0xC0,0x0C,0x20,0x53,0x65 };
+const MSID headObjID=
+    {0x75B22630, 0x668E, 0x11CF, 0xA6,0xD9,0x00,0xAA,0x00,0x62,0xCE,0x6C};
+const MSID dataObjID=
+    {0x75B22636, 0x668E, 0x11CF, 0xA6,0xD9,0x00,0xAA,0x00,0x62,0xCE,0x6C};
+const MSID filePropObjID=
+    {0x8CABDCA1, 0xA947, 0x11CF, 0x8E,0xE4,0x00,0xC0,0x0C,0x20,0x53,0x65};
+const MSID streamPropObjID=
+    {0xB7DC0791, 0xA9B7, 0x11CF, 0x8E,0xE6,0x00,0xC0,0x0C,0x20,0x53,0x65};
 
-const MSID audioStreamObjID =
-{ 0xF8699E40, 0x5B4D, 0x11CF, 0xA8,0xFD,0x00,0x80,0x5F,0x5C,0x44,0x2B };
-const MSID videoStreamObjID =
-{ 0xBC19EFC0, 0x5B4D, 0x11CF, 0xA8,0xFD,0x00,0x80,0x5F,0x5C,0x44,0x2B };
+const MSID audioStreamObjID=
+    {0xF8699E40, 0x5B4D, 0x11CF, 0xA8,0xFD,0x00,0x80,0x5F,0x5C,0x44,0x2B};
+const MSID videoStreamObjID=
+    {0xBC19EFC0, 0x5B4D, 0x11CF, 0xA8,0xFD,0x00,0x80,0x5F,0x5C,0x44,0x2B};
 
-const MSID streamBitrateObjID =
-{ 0x7BF875CE, 0x468D, 0x11D1, 0x8D,0x82,0x00,0x60,0x97,0xC9,0xA2,0xB2 };
-
+const MSID streamBitrateObjID=
+    {0x7BF875CE, 0x468D, 0x11D1, 0x8D,0x82,0x00,0x60,0x97,0xC9,0xA2,0xB2};
 
 // -----------------------------------
 class ASFObject
 {
 public:
-
     enum TYPE
     {
         T_UNKNOWN,
@@ -99,7 +94,7 @@ public:
 
     int getTotalLen()
     {
-        return 24 + dataLen;
+        return 24+dataLen;
     }
 
     unsigned int readHead(Stream &in)
@@ -125,10 +120,9 @@ public:
         id.toString(str);
         LOG_DEBUG("ASF: %s (%s)= %d : %d\n", str.data, getTypeName(), lenLo, lenHi);
 
-
         dataLen = 0;
 
-        return lenLo - 24;
+        return lenLo-24;
     }
 
     void readData(Stream &in, int len)
@@ -141,7 +135,6 @@ public:
         in.read(data, dataLen);
     }
 
-
     void write(Stream &out)
     {
         id.write(out);
@@ -153,28 +146,29 @@ public:
 
     const char *getTypeName()
     {
-        switch (type)
+        switch(type)
         {
-        case T_HEAD_OBJECT:
-            return "ASF_Header_Object";
-        case T_DATA_OBJECT:
-            return "ASF_Data_Object";
-        case T_FILE_PROP:
-            return "ASF_File_Properties_Object";
-        case T_STREAM_PROP:
-            return "ASF_Stream_Properties_Object";
-        case T_STREAM_BITRATE:
-            return "ASF_Stream_Bitrate_Properties_Object";
-        default:
-            return "Unknown_Object";
+            case T_HEAD_OBJECT:
+                return "ASF_Header_Object";
+            case T_DATA_OBJECT:
+                return "ASF_Data_Object";
+            case T_FILE_PROP:
+                return "ASF_File_Properties_Object";
+            case T_STREAM_PROP:
+                return "ASF_Stream_Properties_Object";
+            case T_STREAM_BITRATE:
+                return "ASF_Stream_Bitrate_Properties_Object";
+            default:
+                return "Unknown_Object";
         }
     }
 
-    char data[8192];
-    MSID	id;
-    unsigned int lenLo, lenHi, dataLen;
-    TYPE type;
+    char            data[8192];
+    MSID            id;
+    unsigned int    lenLo, lenHi, dataLen;
+    TYPE            type;
 };
+
 // -----------------------------------
 class ASFStream
 {
@@ -199,18 +193,17 @@ public:
             type = T_UNKNOWN;
 
         in.skip(32);
-        id = in.readShort() & 0x7f;
+        id = in.readShort()&0x7f;
     }
-
 
     const char *getTypeName()
     {
-        switch (type)
+        switch(type)
         {
-        case T_VIDEO:
-            return "Video";
-        case T_AUDIO:
-            return "Audio";
+            case T_VIDEO:
+                return "Video";
+            case T_AUDIO:
+                return "Audio";
         }
         return "Unknown";
     }
@@ -222,9 +215,9 @@ public:
         type = T_UNKNOWN;
     }
 
-    unsigned int id;
-    int bitrate;
-    TYPE type;
+    unsigned int    id;
+    int             bitrate;
+    TYPE            type;
 };
 
 // -----------------------------------
@@ -241,14 +234,14 @@ public:
         numPackets = 0;
         packetSize = 0;
         flags = 0;
-        bitrate = 0;
-        for (int i = 0; i < MAX_STREAMS; i++)
+        bitrate=0;
+        for (int i=0; i<MAX_STREAMS; i++)
             streams[i].reset();
     }
 
-    unsigned int packetSize, numPackets, flags, bitrate;
+    unsigned int    packetSize, numPackets, flags, bitrate;
 
-    ASFStream streams[MAX_STREAMS];
+    ASFStream       streams[MAX_STREAMS];
 };
 
 // -----------------------------------
@@ -264,7 +257,7 @@ public:
         v1 = in.readShort();
         v2 = in.readShort();
 
-        dataLen = len - 8;
+        dataLen = len-8;
         if (dataLen > sizeof(data))
             throw StreamException("ASF chunk too big");
         in.read(data, dataLen);
@@ -280,11 +273,10 @@ public:
         out.write(data, dataLen);
     }
 
-    unsigned int seq, dataLen;
-    unsigned short type, len, v1, v2;
-    unsigned char data[8192];
+    unsigned int    seq, dataLen;
+    unsigned short  type, len, v1, v2;
+    unsigned char   data[8192];
 };
-
 
 #endif
 
