b7060610f80c84493f4babde4b46028c0b7e0953
shinji3 <renrakusaki2003@yahoo.co.jp>
2017-07-18 12:53:17 +0900

    inifile.hを整形

--- core/common/inifile.h
+++ core/common/inifile.h
@@ -2,7 +2,7 @@
 // File : inifile.h
 // Date: 4-apr-2002
 // Author: giles
-// Desc: 
+// Desc:
 //
 // (c) 2002 peercast.org
 // ------------------------------------------------
@@ -26,28 +26,28 @@
 class IniFile
 {
 public:
-    bool	openReadOnly(const char *);
-    bool	openWriteReplace(const char *);
-    void	close();
+    bool        readNext();
 
-    bool	readNext();
+    bool        isName(const char *);
+    char        *getName();
+    int         getIntValue();
+    const char  *getStrValue();
+    bool        getBoolValue();
 
-    bool	isName(const char *);
-    char *	getName();
-    int		getIntValue();
-    char *	getStrValue();
-    bool	getBoolValue();
+    void        writeSection(const char *);
+    void        writeIntValue(const char *, int);
+    void        writeStrValue(const char *, const char *);
+    void        writeBoolValue(const char *, int);
+    void        writeLine(const char *);
 
-    void	writeSection(const char *);
-    void	writeIntValue(const char *, int);
-    void	writeStrValue(const char *, const char *);
-    void	writeBoolValue(const char *, int);
-    void	writeLine(const char *);
+    char        currLine[256];
+    char        *nameStr, *valueStr;
 
+    bool        openReadOnly(const char *);
+    bool        openWriteReplace(const char *);
+    void        close();
 
-    FileStream	fStream;
-    char	currLine[256];
-    char	*nameStr, *valueStr;
+    FileStream  fStream;
 };
 
 #endif
